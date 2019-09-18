088df8de5e3f780f1d83b0fbb18eab6ec6e73fc7
unknown <sbicalho86@gmail.com>
2019-09-18 10:23:52 -0300

    adds menu to base

--- crowdfunding/crowdfunding/urls.py
+++ crowdfunding/crowdfunding/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
-    path('core/', include('core.urls')),
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
